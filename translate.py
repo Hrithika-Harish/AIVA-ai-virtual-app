bb68e2ba0e81f4d6a27e041c976e4fc4ecf71942
Hrithika-Harish <68018738+Hrithika-Harish@users.noreply.github.com>
2023-11-11 14:33:52 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,5 +1,5 @@
 from deep_translator import GoogleTranslator
 
 # Use any translator you like, in this example GoogleTranslator
-translated = GoogleTranslator(source='auto', target='de').translate("Mach weiter so, du bist großartig")
+translated = GoogleTranslator(source='auto', target='en').translate("Mach weiter so, du bist großartig")
 print(translated)
